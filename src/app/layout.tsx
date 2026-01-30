01a056eb06b5e67fd9998ba5afa9a89bf24a5a94
qureshiasif <asifqh2@gmail.com>
2026-01-30 13:00:39 +0530

    added gtag.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6PQXNGE661"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6PQXNGE661');
+            `,
+          }}
+        />
         <meta name="google-adsense-account" content="ca-pub-3152475218574521" />
         {/* Google AdSense */}
         <script
